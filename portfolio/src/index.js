b5a82fa9b01b64ca3aeedb48f1d1055fc1ea4d23
nikhitakilaru <kilarunikhitha@gmail.com>
2025-05-16 16:15:53 +0530

    Add files via upload

---
+++ portfolio/src/index.js
@@ -0,0 +1,51 @@
+// Animate on scroll
+window.sr = ScrollReveal();
+
+sr.reveal('.hero-title', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '20px',
+  delay: 300,
+});
+
+sr.reveal('.hero-cta', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '20px',
+  delay: 600,
+});
+
+sr.reveal('.about-wrapper__info', {
+  duration: 1000,
+  origin: 'left',
+  distance: '20px',
+  delay: 400,
+});
+
+sr.reveal('.about-wrapper__image', {
+  duration: 1000,
+  origin: 'right',
+  distance: '20px',
+  delay: 600,
+});
+
+sr.reveal('.project-wrapper__text', {
+  duration: 1000,
+  origin: 'left',
+  distance: '20px',
+  delay: 500,
+});
+
+sr.reveal('.project-wrapper__image', {
+  duration: 1000,
+  origin: 'right',
+  distance: '20px',
+  delay: 500,
+});
+
+sr.reveal('.contact-wrapper', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '20px',
+  delay: 300,
+});
